b5123b8e45c5aef6ad96961a93b2986f16bcd186
Bryan Eriksson <bryaneriksson3@gmail.com>
2023-04-08 16:41:43 -0700

    Add JS delete confirmation

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,8 @@
     </main>
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
+            integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8="
+            crossorigin="anonymous"></script>
+    <script src="/js/script.js"></script>
 </body>
 </html>
