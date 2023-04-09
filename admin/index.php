aa2399df4b759be008485de603750bf806d64718
Bryan Eriksson <bryaneriksson3@gmail.com>
2023-04-09 14:42:48 -0700

    Button to publish an article

--- admin/index.php
+++ admin/index.php
@@ -37,6 +37,7 @@ $articles = Article::getPage($conn, $paginator->limit, $paginator->offset);
                             <time><?= $article['published_at'] ?></time>
                         <?php else: ?>
                             Unpublished
+                            <button class="publish" data-id="<?= $article['id'] ?>">Publish</button>
                         <?php endif; ?>
                     </td>
                 </tr>
