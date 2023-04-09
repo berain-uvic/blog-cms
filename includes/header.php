5fe28df89dc0ed9fce9b133272a74ce8b861cacc
Bryan Eriksson <bryaneriksson3@gmail.com>
2023-04-09 16:22:39 -0700

    Add Bootstrap

--- includes/header.php
+++ includes/header.php
@@ -3,24 +3,28 @@
 <head>
     <title>My blog</title>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
+
+    <div class="container">
 
     <header>
         <h1>My blog</h1>
     </header>
 
     <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
+        <ul class="nav">
+            <li class="navi-item"><a class="nav-link" href="/">Home</a></li>
             <?php if (Auth::isLoggedIn()) : ?>
                 
-                <li><a href="/admin/">Admin</a></li>
-                <li><a href="/logout.php">Log out</a></li>
+                <li class="navi-item"><a class="nav-link" href="/admin/">Admin</a></li>
+                <li class="navi-item"><a class="nav-link" href="/logout.php">Log out</a></li>
 
             <?php else: ?>
 
-                <li><a href="/login.php">Log in</a></li>
+                <li class="navi-item"><a class="nav-link" href="/login.php">Log in</a></li>
 
             <?php endif; ?>
         </ul>
